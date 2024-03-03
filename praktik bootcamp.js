88402edce7ca13bdcf12c00d05b112ae1b6dd9c5
mfauzian <162025073+mfauzian@users.noreply.github.com>
2024-03-03 19:52:59 +0700

    Add files via upload

---
+++ praktik bootcamp.js
@@ -0,0 +1,79 @@
+//1. Buatlah 5 variable yang dipakai untuk menghitung 3 angkadan outputkan hasilnya :
+// Jawaban :
+console.log('jawaban soal no. 1:')
+let value1 = 5+5+5
+console.log ('hasilnya:',value1) //hasilnya 15
+let value2 = 20-10-5
+console.log ('hasilnya:',value2) //hasilnya 5
+let value3 =  100/2/5
+console.log ('hasilnya:',value3) //hasilnya 10
+let value4 =  5*5*5
+console.log ('hasilnya:',value4) //hasilnya 125
+let value5 =  10+5*2
+console.log ('hasilnya:',value5) //hasilnya 20
+
+//1. buat masing masing 2 dari setiap operator perbandingan, outputkan hasilnya
+
+// Jawaban :
+console.log('')
+console.log('jawaban soal no. 2:')
+//Operator Perbandingan ' == ' 
+let perbandingan1 = 5 == 5
+console.log('hasil dari perbandingan nilai : 5 == 5 adalah',perbandingan1)
+let perbandingan2 = 8 == 5
+console.log('hasil dari perbandingan nilai : 8 == 5 adalah',perbandingan2)
+
+//Operator Perbandingan ' === ' 
+console.log('')
+let perbandingan3 = 2 === 2
+console.log('hasil dari perbandingan nilai : 2 === 2 adalah',perbandingan3)
+let perbandingan4 = 2 === "2"
+console.log('hasil dari perbandingan nilai : 2 === "2" adalah',perbandingan4)
+
+//Operator Perbandingan ' != ' 
+console.log('')
+let perbandingan5 = 2 != 3
+console.log('hasil dari perbandingan nilai : 2 != 3 adalah',perbandingan5)
+let perbandingan6 = 2 != 2
+console.log('hasil dari perbandingan nilai : 2 != 2 adalah',perbandingan6)
+
+
+//Operator Perbandingan ' !== ' 
+console.log('')
+let perbandingan7 = 2 !== 3
+console.log('hasil dari perbandingan nilai : 2 !== 3 adalah',perbandingan7)
+let perbandingan8 = 2 !== 2
+console.log('hasil dari perbandingan nilai : 2 !== 2 adalah',perbandingan8)
+let perbandingan9 = 2 !== "2"
+console.log('hasil dari perbandingan nilai : 2 !== "2" adalah',perbandingan9)
+
+//Operator Perbandingan ' > ' 
+console.log('')
+let perbandingan10 = 15 > 10
+console.log('hasil dari perbandingan nilai : 15 > 10 adalah',perbandingan10)
+let perbandingan11 = 15 > 15
+console.log('hasil dari perbandingan nilai : 15 > 15 adalah',perbandingan11)
+
+//Operator Perbandingan ' >= ' 
+console.log('')
+let perbandingan12 = 15 >= 10
+console.log('hasil dari perbandingan nilai : 15 >= 10 adalah',perbandingan12)
+let perbandingan13 = 15 >= 15
+console.log('hasil dari perbandingan nilai : 15 >= 15 adalah',perbandingan13)
+
+
+
+//Operator Perbandingan ' < ' 
+console.log('')
+let perbandingan15 = 18 < 20
+console.log('hasil dari perbandingan nilai : 18 < 20 adalah',perbandingan15)
+let perbandingan16 = 20 < 20
+console.log('hasil dari perbandingan nilai : 20 < 20 adalah',perbandingan16)
+
+//Operator Perbandingan ' <= ' 
+console.log('')
+let perbandingan17 = 18 <= 20
+console.log('hasil dari perbandingan nilai : 18 <= 20 adalah',perbandingan17)
+let perbandingan18 = 20 <= 20
+console.log('hasil dari perbandingan nilai : 20 <= 20 adalah',perbandingan18)
+
